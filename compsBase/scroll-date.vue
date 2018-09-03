89690bc6f81e9cc3e51c188c8dfde771b166e14e
kaiqiw <1132620342@qq.com>
2018-09-03 17:57:37 +0800

    scroll-date

---
+++ compsBase/scroll-date.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>
+            <input type="text"  :value="formatDate">
+        </div>
+        <div>
+            <select name="" id="" v-model="value1">
+                <option v-for="item in arr1" :key="item" :value="item">{{item}}</option>
+            </select>
+            <select name="" id="" v-model="value2">
+                <option v-for="item in arr2" :key="item" :value="item">{{item}}</option>
+            </select>
+            <select name="" id="" v-model="value3">
+                <option v-for="item in arr3" :key="item" :value="item">{{item}}</option>
+            </select>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr1: [2018, 2019, 2020],
+      value1: '',
+      arr2: [12, 11, 10],
+      value2: '',
+      arr3: [1, 2, 3],
+      value3: ''
+    }
+  },
+  computed:{
+      formatDate(){
+          return this.value1+'/'+this.value2+'/'+this.value3
+      }
+  }
+}
+</script>
+
+<style>
+</style>
