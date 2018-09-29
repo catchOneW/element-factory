d81e2b1e891ebfac8805284a2c50909145b15586
kaiqiw <1132620342@qq.com>
2018-09-29 10:49:44 +0800

    date ISOString

---
+++ src/js/date.js
@@ -0,0 +1,22 @@
+// var a=new Date()
+// undefined
+// a
+// Sat Sep 29 2018 10:45:22 GMT+0800 (中国标准时间)
+// a.toDateString()
+// "Sat Sep 29 2018"
+// a.toGMTString()
+// "Sat, 29 Sep 2018 02:45:22 GMT"
+// a.toISOString()
+// "2018-09-29T02:45:22.381Z"
+// a.toLocaleDateString()
+// "2018/9/29"
+// a.toLocaleString()
+// "2018/9/29 上午10:45:22"
+// a.toLocaleTimeString()
+// "上午10:45:22"
+// a.toString()
+// "Sat Sep 29 2018 10:45:22 GMT+0800 (中国标准时间)"
+// a.toTimeString()
+// "10:45:22 GMT+0800 (中国标准时间)"
+// a.toUTCString()
+// "Sat, 29 Sep 2018 02:45:22 GMT"
