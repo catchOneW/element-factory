7b5bbdeaac080b995e6cd8789c357c47d66b1d9b
kaiqiw <1132620342@qq.com>
2018-10-09 22:13:50 +0800

    down-move-up,  enter-move-leave

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,52 @@
     <button @click="submit">提交</button> -->
     <!-- <tableRowSpan></tableRowSpan> -->
     <!-- <permision></permision> -->
-    <mouseRight></mouseRight>
+    <!-- <mouseRight></mouseRight> -->
+    1111
   </div>
 </template>
 <script>
+import { Observable } from 'rxjs'
+
 export default {
   data() {
     return {
       urls: ['']
     }
   },
-  methods: {
-    submit() {
-      console.log(this.$refs.MutiRef.fileList)
-      let rawFileList = this.$refs.MutiRef.fileList.filter(x => x.raw)
-      let urlFileList = this.$refs.MutiRef.fileList.filter(x => !x.raw)
-    },
-    open(file) {
-      this.$refs.MutiRef.fileList = this.urls.map((e, index) => {
-        return {
-          name: index,
-          url: e
-        }
+  created() {
+    const dragDOM = document.getElementById('drag')
+    const mouseDown = Rx.Observable.fromEvent(dragDOM, 'mouseenter')
+    const mouseUp = Rx.Observable.fromEvent(dragDOM, 'mouseleave')
+    const mouseMove = Rx.Observable.fromEvent(dragDOM, 'mousemove')
+    mouseDown
+      .map(event => mouseMove.takeUntil(mouseUp))
+      .concatAll()
+      .subscribe(e => {
+        console.log(e)
       })
-    }
+  },
+  methods: {
+    // submit() {
+    //   console.log(this.$refs.MutiRef.fileList)
+    //   let rawFileList = this.$refs.MutiRef.fileList.filter(x => x.raw)
+    //   let urlFileList = this.$refs.MutiRef.fileList.filter(x => !x.raw)
+    // },
+    // open(file) {
+    //   this.$refs.MutiRef.fileList = this.urls.map((e, index) => {
+    //     return {
+    //       name: index,
+    //       url: e
+    //     }
+    //   })
+    // }
   }
 }
 </script>
 <style lang="scss">
 @import './scss/index.scss';
+
+#app {
+  height: 100vh;
+}
 </style>
